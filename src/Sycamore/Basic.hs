5d2281bd40a078ddd4e40467414642ab9c27dcb8
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-07-19 16:40:01 +0300

    remove reference to deleted source file

---
+++ src/Sycamore/Basic.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+
+module Basic where 
+
+import           Control.Monad        hiding (fmap)
+import           Data.Aeson           (ToJSON, FromJSON)
+import           Data.List.NonEmpty   (NonEmpty (..))
+import           Data.Map             as Map
+import           Data.Text            (pack, Text)
+import           GHC.Generics         (Generic)
+import           Ledger               hiding (singleton)
+import qualified Ledger.Constraints   as Constraints
+import qualified Ledger.Typed.Scripts as Scripts
+import           Ledger.Value         as Value
+import           Ledger.Ada           as Ada
+import           Playground.Contract  (IO, ensureKnownCurrencies, printSchemas, stage, printJson)
+import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types     (KnownCurrency (..))
+import           Plutus.Contract
+import qualified PlutusTx
+import           PlutusTx.Prelude     hiding (unless)
+import qualified Prelude              as P
+import           Schema               (ToSchema)
+import           Text.Printf          (printf)
+
+--this pragma allows the compiler to inline the definition of `mkValidator` inside the `||` brackets
+--Any function that is to be used for on-chain code will need this validator.
+{-# INLINABLE mkValidator -#}
+--this function will be supplied to `mkValidator` which will compile it into Plutus Core to produce a Validator.
+mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkValidator _ _ _ = ()
+
+
+--The `$$` will take the syntax tree and splice it into this part of the Haskell code.
+--The oxford brackets `||` convert the compiled Plutus Core code into a syntax tree.
+validator :: Validator
+validator = mkValidatorScript $$(PlutusTx.compile [ || mkValidator || ])
