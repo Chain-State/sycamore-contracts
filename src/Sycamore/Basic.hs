d9c59a5872c05f1d533ed7ffe410062e372e7dbf
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-07-19 16:50:54 +0300

    add new test validator

--- src/Sycamore/Basic.hs
+++ src/Sycamore/Basic.hs
@@ -25,7 +25,7 @@ import           Data.Text            (pack, Text)
 import           GHC.Generics         (Generic)
 import           Ledger               hiding (singleton)
 import qualified Ledger.Constraints   as Constraints
-import qualified Ledger.Typed.Scripts as Scripts
+import qualified Ledger.Scripts as    Scripts
 import           Ledger.Value         as Value
 import           Ledger.Ada           as Ada
 import           Playground.Contract  (IO, ensureKnownCurrencies, printSchemas, stage, printJson)
@@ -40,7 +40,7 @@ import           Text.Printf          (printf)
 
 --this pragma allows the compiler to inline the definition of `mkValidator` inside the `||` brackets
 --Any function that is to be used for on-chain code will need this validator.
-{-# INLINABLE mkValidator -#}
+{-# INLINABLE mkValidator #-}
 --this function will be supplied to `mkValidator` which will compile it into Plutus Core to produce a Validator.
 mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkValidator _ _ _ = ()
@@ -49,4 +49,12 @@ mkValidator _ _ _ = ()
 --The `$$` will take the syntax tree and splice it into this part of the Haskell code.
 --The oxford brackets `||` convert the compiled Plutus Core code into a syntax tree.
 validator :: Validator
-validator = mkValidatorScript $$(PlutusTx.compile [ || mkValidator || ])
+validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+
+--generate hash of the validator
+validatorHash :: Ledger.ValidatorHash
+validatorHash = Scripts.validatorHash validator
+
+--generate address from the validator
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
