7cca03654c5f8a0020a8ab352d8055be9ebe647a
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-07-20 12:42:26 +0300

    refactor and offchain additions on validator

--- src/Sycamore/Basic.hs
+++ src/Sycamore/Basic.hs
@@ -22,26 +22,28 @@ import           Data.Aeson           (ToJSON, FromJSON)
 import           Data.List.NonEmpty   (NonEmpty (..))
 import           Data.Map             as Map
 import           Data.Text            (pack, Text)
+import           Data.Void 
 import           GHC.Generics         (Generic)
+import           Prelude              (IO, Semigroup (..), String, undefined) 
 import           Ledger               hiding (singleton)
-import qualified Ledger.Constraints   as Constraints
+import           Ledger.Constraints   as Constraints
 import qualified Ledger.Scripts as    Scripts
 import           Ledger.Value         as Value
 import           Ledger.Ada           as Ada
 import           Playground.Contract  (IO, ensureKnownCurrencies, printSchemas, stage, printJson)
 import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
 import           Playground.Types     (KnownCurrency (..))
-import           Plutus.Contract
+import           Plutus.Contract      
 import qualified PlutusTx
-import           PlutusTx.Prelude     hiding (unless)
-import qualified Prelude              as P
+import qualified PlutusTx.Builtins    as BuiltIns
+import           PlutusTx.Prelude     hiding (Semigroup(..),unless)
 import           Schema               (ToSchema)
 import           Text.Printf          (printf)
 
 --this pragma allows the compiler to inline the definition of `mkValidator` inside the `||` brackets
 --Any function that is to be used for on-chain code will need this validator.
 {-# INLINABLE mkValidator #-}
---this function will be supplied to `mkValidator` which will compile it into Plutus Core to produce a Validator.
+--this function will be supplied to `mkValidator` which will compile it into Plutus Core to lockToContract a Validator.
 mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkValidator _ _ _ = ()
 
@@ -52,9 +54,45 @@ validator :: Validator
 validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
 
 --generate hash of the validator
-validatorHash :: Ledger.ValidatorHash
-validatorHash = Scripts.validatorHash validator
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash validator
 
 --generate address from the validator
 scrAddress :: Ledger.Address
 scrAddress = scriptAddress validator
+
+
+--Define endpoints (Functions that will allow users to enter data and trigger actions on the validators)
+
+type BasicSchema = Endpoint "lockToContract" Integer
+            .\/    Endpoint "unlockFromContract" ()
+
+lockToContract :: AsContractError e => Integer -> Contract w s e ()
+lockToContract amount = do
+    let tx = mustPayToOtherScript valHash (Datum $ BuiltIns.mkI 0) $ Ada.lovelaceValueOf amount
+    ledgerTx <- submitTx tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    logInfo @String $ printf "Sent %d Ada to contract" amount
+
+
+unlockFromContract :: forall w s e. AsContractError e => Contract w s e ()
+unlockFromContract = do
+    utxos <- utxosAt scrAddress
+    let orefs = fst <$> Map.toList utxos 
+        lookups = Constraints.unspentOutputs utxos <>
+                  Constraints.otherScript validator
+        tx :: Constraints.TxConstraints Void Void
+        tx = mconcat [mustSpendScriptOutput oref $ Redeemer $ BuiltIns.mkI 17 | oref <- orefs]
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    logInfo @String $ "Ada amount collected"
+
+endpoints :: Contract () BasicSchema Text ()
+endpoints = awaitPromise (lockToContract' `select` unlockFromContract') >> endpoints
+  where 
+    lockToContract' = endpoint @"lockToContract" lockToContract
+    unlockFromContract' = endpoint @"unlockFromContract" $ const unlockFromContract
+
+mkSchemaDefinitions ''BasicSchema
+
+mkKnownCurrencies []
