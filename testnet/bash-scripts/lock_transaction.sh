074ff81b1be30ee6e0b0c9d48e442d2fbd4a65d5
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-08-19 08:34:40 +0300

    test:lock tokens to basic script

--- testnet/bash-scripts/lock_transaction.sh
+++ testnet/bash-scripts/lock_transaction.sh
@@ -1,15 +1,15 @@
 cardano-cli transaction build \
     --babbage-era \
     --testnet-magic 1097911063 \
-    --change-address $(cat ~/Projects/de_afyarekod/sycamore-plutus/dev-wallets/wallet1/w1.addr) \
-    --tx-in 971c67188141915d8029a4b8fe2a36258d5c6f9a6ab631f885dcad46d2fc0821#0 \
-    --tx-out "$(cat basic-script.addr) 20000000 lovelace" \
-    --tx-out-datum-hash-file unit.json \
+    --change-address $(cat ~/projects/ar/sycamore-contracts/testnet/wallets/wallet1/w1.addr) \
+    --tx-in 5a0ca3bd87f23ed319df33671634f559b46294455813d52efabad902bde97dc7#1 \
+    --tx-out "$(cat ../basic-validator/basic-script.addr) 5000000 lovelace + 17 4d874093ef3a3449e33440a4bd8631077458ce125d1963bdfc402c86.42616e6461" \
+    --tx-out-datum-hash-file ../basic-validator/unit.json \
     --out-file tx.body
 
 cardano-cli transaction sign \
     --tx-body-file tx.body \
-    --signing-key-file ~/Projects/de_afyarekod/sycamore-plutus/dev-wallets/wallet1/w1.skey \
+    --signing-key-file  ~/projects/ar/sycamore-contracts/testnet/wallets/wallet1/w1.skey \
     --testnet-magic 1097911063 \
     --out-file tx.signed
 
