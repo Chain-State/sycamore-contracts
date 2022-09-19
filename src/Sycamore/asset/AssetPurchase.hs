05fe63573bb6239e6b351f92b1dafcc997016848
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-09-19 16:42:40 +0300

    syc-45: rename subdir and files for real sc

---
+++ src/Sycamore/asset/AssetPurchase.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+
+module Sycamore.AssetPurchase where 
+
+import           Control.Monad        hiding (fmap)
+import           Data.List.NonEmpty   (NonEmpty (..))
+import           Data.Map             as Map
+import           Data.Text            (pack, Text)
+import           Data.Void 
+import           Prelude              (IO, Semigroup (..), String, undefined) 
+import           Ledger               hiding (singleton)
+import           Ledger.Constraints   as Constraints
+import qualified Ledger.Typed.Scripts as    Scripts
+import           Ledger.Ada           as Ada
+import           Plutus.Contract      
+import           PlutusTx             (Data (..))
+import qualified PlutusTx
+import           PlutusTx.Prelude     hiding (Semigroup(..),unless)
+import           Schema               (ToSchema)
+import           Text.Printf          (printf)
+
+newtype DataAccessRedeemer = DataAccessRedeemer Integer
+
+--make above custom data type into an instance of `IsData` (so that can be used in validators by to & from BuildInData)
+-- Uses template Haskell for making the instance.('' on the type makes it the parameter).
+PlutusTx.unstableMakeIsData ''DataAccessRedeemer
+
+--this pragma allows the compiler to inline the definition of `mkValidator` inside the `||` brackets
+--Any function that is to be used for on-chain code will need this validator.
+{-# INLINABLE mkValidator #-}
+--this function will be supplied to `mkValidator` which will compile it into Plutus Core to lockToContract a Validator.
+mkValidator :: () -> DataAccessRedeemer -> ScriptContext -> Bool 
+mkValidator _ (DataAccessRedeemer r) _ = traceIfFalse "Wrong Redeemer" $ r == 42
+
+--wtih typed data more boiler-plate code is required
+data Typed
+instance Scripts.ValidatorTypes Typed where
+    type instance DatumType Typed = ()
+    type instance RedeemerType Typed = DataAccessRedeemer 
+
+typedValidator :: Scripts.TypedValidator Typed
+typedValidator = Scripts.mkTypedValidator @Typed
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @() @DataAccessRedeemer
+
+validator :: Validator
+validator = Scripts.validatorScript typedValidator
+
+--generate hash of the validator
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash typedValidator
+
+--generate address from the validator
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
+
