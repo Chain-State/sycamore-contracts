78df9877f902fb6a49a81e9c216c5c875acff177
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-04-11 17:17:48 +0300

    unlock nft minted directly to validator address

--- testnet/scripts/unlockTokenFromScript.sh
+++ testnet/scripts/unlockTokenFromScript.sh
@@ -1,13 +1,13 @@
 cardano-cli transaction build \
      --babbage-era \
-    --tx-in deefa95c8a926fc0cff2e4da92c5cb01c9ee0c32281985805225a460c78ba425#0 \
+    --tx-in e2a472f03a6b59cb43decb00733c3d5ef45537968cfca4c71da90771f7009cf8#0 \
     --tx-in-datum-file "../upp/unlock-script/unit.json" \
     --tx-in-redeemer-file "../upp/unlock-script/unit.json" \
-    --tx-in-script-file "../upp/lock-script/uppv2_test02.plutus" \
-    --tx-out $(cat ../wallets/buyer/buyer.addr)+"1500000 lovelace + 1 d6090a22c2b52c2c5754a9d4a433b13522efda95a72b56287521f8a8.4152233139" \
+    --tx-in-script-file "../upp/lock-script/uppv2_test05.plutus" \
+    --tx-out $(cat ../wallets/buyer/buyer.addr)+"1500000 lovelace + 1 93f0126201070494884c86bb0aca2b5d2823107e55b69dc85344d28b.4152233232" \
     --tx-out $(cat ../wallets/beneficiary/beneficiary.addr)+2000000 \
     --tx-out $(cat ../wallets/minter/minter.addr)+2000000 \
-    --tx-in-collateral "161be5322f5b0e2ddb5045a99629e23d902b12408bcee6f494f646841f930207#0" \
+    --tx-in-collateral "285ad96cb21bb169fde23c97ba75de040bb2832f3d560e704a3c7edeb0f4bf4e#3" \
     --change-address $(cat ../wallets/buyer/buyer.addr) \
     --protocol-params-file "../upp/unlock-script/protocol-params.json" \
     --out-file "../upp/unlock-script/txs/tx.body" \
