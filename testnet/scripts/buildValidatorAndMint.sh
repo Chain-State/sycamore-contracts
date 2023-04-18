6cf2165e778fabd11c3d3c0bcfdb794c5e9a5a4f
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-04-18 11:34:05 +0300

    WIP: Working benefator params for script generation with addresss

--- testnet/scripts/buildValidatorAndMint.sh
+++ testnet/scripts/buildValidatorAndMint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 ASSET_TITLE=$1
+PUBLISHER_PHK=$2
+BENEFACTOR1_PHK=$3
+BENEFACTOR2_PHK=$4
+BENEFACTOR3_PHK=$5
 WORK_DIR=/home/prometheus/projects/afyarekod-upp/sycamore-contracts
 APP_DIR=/home/prometheus/projects/plutus-apps
 
 #Drop into nix shell
 export LD_LIBRARY_PATH=''
 cd $APP_DIR
-nix-shell --command "cd ${WORK_DIR} && echo $PWD && cabal exec serialise-validator ${ASSET_TITLE}"
+nix-shell --command "cd ${WORK_DIR} && echo $PWD && cabal exec serialise-validator ${ASSET_TITLE} ${PUBLISHER_PHK} ${BENEFACTOR1_PHK} ${BENEFACTOR2_PHK} ${BENEFACTOR3_PHK} && cd ${WORK_DIR}/testnet/scripts && ./generateScriptAddress ${ASSET_TITLE}"
 
