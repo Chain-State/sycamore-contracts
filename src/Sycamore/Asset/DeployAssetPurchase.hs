382b84d070b95313c2e7e030c66dbaf63668db92
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-05-13 17:24:25 +0300

    Wip: Refactor validator serializer

--- src/Sycamore/Asset/DeployAssetPurchase.hs
+++ src/Sycamore/Asset/DeployAssetPurchase.hs
@@ -1,64 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Sycamore.Asset.DeployAssetPurchase
-    ( writeJSON
-    , writeValidator
-    , writeAssetPurchaseValidator
-    , writeUnit
-    , writeRedeemer
+    ( 
+        -- writeJSON
+     writeAssetPurchaseValidator
+    -- , writeUnit
+    -- , writeRedeemer
     ) where
 
-import           Cardano.Api
-import           Cardano.Api.Shelley          (PlutusScript (..))
-import           Codec.Serialise              (serialise)
-import           Data.Aeson                   (encode)
-import qualified Data.ByteString.Lazy         as LBS
-import qualified Data.ByteString.Short        as SBS
-import qualified Data.Text                    as T
-import qualified Data.Text.Encoding           as TE
+
 
 import qualified Data.String                  as DS
-import           Plutus.Script.Utils.Value    as Val
-import qualified Plutus.V2.Ledger.Api         as L
-import           PlutusTx                     (Data (..))
-import qualified PlutusTx
+
+import           Cardano.Api           (Error (displayError), PlutusScript,
+                                        PlutusScriptV2, prettyPrintJSON,
+                                        writeFileJSON, writeFileTextEnvelope)
+import           Cardano.Api.Shelley   (PlutusScript (..), fromPlutusData,
+                                        scriptDataToJsonDetailedSchema)
+import           Codec.Serialise       (Serialise, serialise)
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy  as BSL
+import qualified Data.ByteString.Short as BSS
+import Plutus.V2.Ledger.Api (TokenName(..), Validator(..), CurrencySymbol(..), PubKeyHash(..),getLedgerBytes)
+import           Plutus.Script.Utils.Value (assetClass)
+
+import           PlutusTx              (CompiledCode)
+
 
 import           Sycamore.Asset.AssetPurchase
 
-dataToScriptData :: Data -> ScriptData
-dataToScriptData (Constr n xs) = ScriptDataConstructor n $ dataToScriptData <$> xs
-dataToScriptData (Map xs)      = ScriptDataMap [(dataToScriptData x, dataToScriptData y) | (x, y) <- xs]
-dataToScriptData (List xs)     = ScriptDataList $ dataToScriptData <$> xs
-dataToScriptData (I n)         = ScriptDataNumber n
-dataToScriptData (B bs)        = ScriptDataBytes bs
 
-writeJSON :: PlutusTx.ToData a => FilePath -> a -> IO ()
-writeJSON file = LBS.writeFile file . encode . scriptDataToJson ScriptDataJsonDetailedSchema . dataToScriptData . PlutusTx.toData
+-- writeValidator :: FilePath -> L.Validator -> IO (Either (FileError ()) ())
+-- writeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV2) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . L.unValidatorScript
 
-writeValidator :: FilePath -> L.Validator -> IO (Either (FileError ()) ())
-writeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV2) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . L.unValidatorScript
+-- writeUnit :: IO ()
+-- writeUnit = writeJSON "testnet/upp/lock-script/unit.json" ()
 
-writeUnit :: IO ()
-writeUnit = writeJSON "testnet/upp/lock-script/unit.json" ()
+-- writeRedeemer :: IO ()
+-- writeRedeemer = writeJSON "testnet/upp/lock-script/redeemer.json" ()
 
-writeRedeemer :: IO ()
-writeRedeemer = writeJSON "testnet/upp/lock-script/redeemer.json" ()
+serializableToScript :: Serialise a => a -> PlutusScript PlutusScriptV2
+serializableToScript = PlutusScriptSerialised . BSS.toShort . BSL.toStrict . serialise
 
-makeList :: [String] -> [L.PubKeyHash]
-makeList =  map (L.PubKeyHash . L.getLedgerBytes . DS.fromString)
+-- Serialize validator
+validatorToScript :: Validator -> PlutusScript PlutusScriptV2
+validatorToScript = serializableToScript
 
-writeAssetPurchaseValidator :: String -> String -> String -> [String] -> IO (Either (FileError ()) ())
-writeAssetPurchaseValidator file assetName publisherPkh beneficiaryPbkhs = writeValidator file $ validator $ AssetPurchase
+-- Create file with Plutus script
+writeScriptToFile :: FilePath -> PlutusScript PlutusScriptV2 -> IO ()
+writeScriptToFile filePath script =
+  writeFileTextEnvelope filePath Nothing script >>= \case
+    Left err -> print $ displayError err
+    Right () -> putStrLn $ "Serialized script to: " ++ filePath
+
+-- Create file with compiled Plutus validator
+writeValidatorToFile :: FilePath -> Validator -> IO ()
+writeValidatorToFile filePath = writeScriptToFile filePath . validatorToScript
+
+makeList :: [String] -> [PubKeyHash]
+makeList =  map (PubKeyHash . getLedgerBytes . DS.fromString)
+
+writeAssetPurchaseValidator :: String -> String -> String -> [String] -> IO ()
+writeAssetPurchaseValidator filePath assetName publisherPkh beneficiaryPbkhs = writeValidatorToFile filePath $ validator $ AssetPurchase
                                 {
-                                    saleNftTn = Val.tokenName $ TE.encodeUtf8 $ T.pack assetName
-                                   ,minter = L.PubKeyHash $ L.getLedgerBytes $ DS.fromString publisherPkh
-                                   ,minterCurrency = Val.assetClass (Val.currencySymbol "") (Val.tokenName  $ TE.encodeUtf8 $ T.pack "")
+                                    saleNftTn = TokenName $  getLedgerBytes $ DS.fromString assetName
+                                   ,minter = PubKeyHash $ getLedgerBytes $ DS.fromString publisherPkh
+                                   ,minterCurrency = assetClass (CurrencySymbol "") (TokenName  $  getLedgerBytes $ DS.fromString "")
                                    ,minterAmount = 2000000
                                    ,beneficiary = makeList beneficiaryPbkhs
-                                   ,beneficiaryCurrency = Val.assetClass (Val.currencySymbol "") (Val.tokenName  $ TE.encodeUtf8 $ T.pack "")
+                                   ,beneficiaryCurrency = assetClass (CurrencySymbol "") (TokenName  $  getLedgerBytes $ DS.fromString "")
                                    ,beneficiaryAmount = 2000000
-                                   ,collateral = Val.assetClass (Val.currencySymbol "") (Val.tokenName $ TE.encodeUtf8 $ T.pack "")
+                                   ,collateral = assetClass (CurrencySymbol "") (TokenName  $  getLedgerBytes $ DS.fromString "")
                                    ,collateralAmnt = 2000000
-                                   ,saleExpiresOn = 1680952180000
+                                   ,saleExpiresOn =1735689600 
                                 }
